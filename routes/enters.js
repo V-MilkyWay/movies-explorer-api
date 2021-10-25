949042cbe5689c13d7b41d7c3c31d8268ab2f5a4
Vladislav Milushkov <radshura@yandex.ru>
2021-10-25 17:40:41 +0300

    Backend

---
+++ routes/enters.js
@@ -0,0 +1,24 @@
+
+const { celebrate, Joi } = require('celebrate');
+// создадим express router
+const routerEnters = require('express').Router();
+// импортируем controllers
+const { createUser, login } = require('../controllers/users');
+
+routerEnters.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
+
+routerEnters.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+
+// экспортируем router
+module.exports = routerEnters;
